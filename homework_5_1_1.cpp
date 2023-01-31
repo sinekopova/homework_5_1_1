d2de01403f1f0fb84d50d584352e6c587bc804df
Valentina Baranova <v.sinekopova@gmail.com>
2023-01-31 22:49:22 +0300

    Initial commit

---
+++ homework_5_1_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> v = { 4, 7, 9, 14, 12 };
+
+    std::cout << "Входные данные: ";
+    std::for_each(v.begin(), v.end(), [](const int& val) { std::cout << val << " "; });
+    std::cout << "\n";    
+
+
+    auto mult = [](int& val) { val = (val % 2 == 0) ? val : val * 3; 
+                               std::cout << val << " ";
+    };
+    std::cout << "Выходные данные: ";    
+    std::for_each(v.begin(), v.end(), mult );
+    std::cout << "\n";
+}
